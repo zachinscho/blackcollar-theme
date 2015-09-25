5f27c201a688b91623dcf86a949e5c73461eee2f
Zach Inscho <zach@drxlr.com>
2015-09-25 01:31:43 -0400

    setup

---
+++ footer.php
@@ -0,0 +1,19 @@
+		 <!-- jQuery -->
+	    <script src="js/jquery.js"></script>
+	
+	    <!-- Bootstrap Core JavaScript -->
+	    <script src="js/bootstrap.min.js"></script>
+	
+	    <!-- Plugin JavaScript -->
+	    <script src="js/jquery.easing.min.js"></script>
+	    <script src="js/jquery.fittext.js"></script>
+	    <script src="js/wow.min.js"></script>
+	
+	    <!-- Custom Theme JavaScript -->
+	    <script src="js/creative.js"></script>
+		<?php // all js scripts are loaded in library/bones.php ?>
+		<?php wp_footer(); ?>
+
+	</body>
+
+</html> <!-- end of site. what a ride! -->
---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Author: Zach Inscho
+URL: http://zachinscho.com/
+*/
+?>

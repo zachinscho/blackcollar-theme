e686763bf99353cbf40dff3532d2a62db519b76c
Zach Inscho <zach@drxlr.com>
2015-09-29 16:23:44 -0400

    9.29 design/content updates

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
             <div class="header-content-inner">
                 <!--<h1>Your Favorite Source of Free Bootstrap Themes</h1>
                 <hr>-->
-                <p><img src="<?php  echo $templateDirectory;  ?>/library/images/black-collar-logo.png" alt=""/></p>
+                <p><img src="<?php  echo $templateDirectory;  ?>/library/images/black-collar-logo.svg" alt=""/></p>
                 <!--<p>Vivamus et lorem in ligula efficitur porta et eu leo. Quisque accumsan leo et ipsum scelerisque interdum.</p>-->
                 <a href="#about" class="btn btn-primary btn-xl page-scroll">Learn About Us</a>
             </div>
@@ -20,8 +20,9 @@
                 <div class="col-lg-8 col-lg-offset-2 text-center">
                     <h2 class="section-heading">About Black Collar</h2>
                     <hr class="light">
-                    <p class="text-faded">We're a full service DIY screen printing shop located in Baltimore, MD. We offer everything from basic t-shirt printing to album packaging, posters and Eco-friendly printing.</p>
-                    <a href="#" class="btn btn-default btn-xl">Place an Order</a>
+                    <p class="text-faded">We're a full service DIY screen printing shop located in Baltimore, MD. We offer everything from basic t-shirt printing to album packaging, posters and eco-friendly printing.</p>
+                    <a href="#" class="btn btn-default btn-xl">Start an Order</a>
+                    <a href="#" class="btn btn-default btn-xl">Our Process</a>
                 </div>
             </div>
         </div>
@@ -40,7 +41,7 @@
             <div class="row">
                 <div class="col-lg-3 col-md-6 text-center">
                     <div class="service-box">
-                        <i class="fa fa-4x fa-diamond wow bounceIn text-primary"></i>
+                        <i class="fa fa-4x fa-star-o wow bounceIn text-primary"></i>
                         <h3>T-Shirts</h3>
                         <p class="text-muted">We have lots of options to choose from including USA and Union made shirts.</p>
                         <a href="#tshirt-rates">View Rates</a> 
@@ -48,7 +49,7 @@
                 </div>
                 <div class="col-lg-3 col-md-6 text-center">
                     <div class="service-box">
-                        <i class="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".1s"></i>
+                        <i class="fa fa-4x fa-soccer-ball-o wow bounceIn text-primary" data-wow-delay=".1s"></i>
                         <h3>More Apparel</h3>
                         <p class="text-muted">We've got Hoodies, Jackets/Windbreakers, Atheletic Apparel, Tanks and more.</p>
                         <a href="#moreapparel-rates">View Rates</a> 
@@ -56,7 +57,7 @@
                 </div>
                 <div class="col-lg-3 col-md-6 text-center">
                     <div class="service-box">
-                        <i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
+                        <i class="fa fa-4x fa-music wow bounceIn text-primary" data-wow-delay=".2s"></i>
                         <h3>Posters and Covers</h3>
                         <p class="text-muted">We can print show posters and the cover packaging for your album art as well.</p>
                         <a href="#poster-rates">View Rates</a> 
@@ -78,7 +79,7 @@
         <div class="container-fluid">
             <div class="row no-gutter">
                 <div class="col-lg-4 col-sm-6">
-                    <a href="#" class="portfolio-box">
+                    <a href="#portfolioModal1" class="portfolio-box" data-toggle="modal">
                         <img src="<?php  echo $templateDirectory;  ?>/library/images/portfolio/bc-port-1.jpg" class="img-responsive" alt="">
                         <div class="portfolio-box-caption">
                             <div class="portfolio-box-caption-content">
@@ -199,6 +200,40 @@
             </div>
         </div>
     </section>
+    
+     <!-- Portfolio Modal 1 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <!-- Project Details Go Here -->
+                            <h2>Project Name</h2>
+                            <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                            <img class="img-responsive img-centered" src="img/portfolio/roundicons-free.png" alt="">
+                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                            <p>
+                                <strong>Want these icons in this portfolio item sample?</strong>You can download 60 of them for free, courtesy of <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">RoundIcons.com</a>, or you can purchase the 1500 icon set <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">here</a>.</p>
+                            <ul class="list-inline">
+                                <li>Date: July 2014</li>
+                                <li>Client: Round Icons</li>
+                                <li>Category: Graphic Design</li>
+                            </ul>
+                            <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 
 <?php get_footer(); ?>
